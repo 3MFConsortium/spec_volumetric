708c1d41c3c7a2ee5f453724c12013cff7d82cca
Jan Orend <56254096+3dJan@users.noreply.github.com>
2024-11-25 14:14:46 +0100

    Update wkhtmltopdf installation script to version 0.12.6

--- markdownToPDF/install_wkhtmltopdf.sh
+++ markdownToPDF/install_wkhtmltopdf.sh
@@ -23,7 +23,7 @@
 
 #!/bin/bash
 
-wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
-tar vxf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.6/wkhtmltox-0.12.6_linux-generic-amd64.tar.xz
+tar vxf wkhtmltox-0.12.6_linux-generic-amd64.tar.xz
 cp wkhtmltox/bin/wkhtmltopdf .
 chmod a+x wkhtmltopdf
